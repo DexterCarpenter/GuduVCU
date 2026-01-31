ece6d800e48df593f917440dc0b7eda4fd186cb3
Milan Donhowe <milanaugust@gmail.com>
2026-01-31 12:53:31 -0800

    beep boop going to rebase soon

--- main/vcu_main.c
+++ main/vcu_main.c
@@ -8,11 +8,27 @@
 #include "esp_chip_info.h"
 #include "esp_flash.h"
 #include "esp_system.h"
+#include "driver/gpio.h"
 
+
+// initialize all hardware
+void config_gpio(){
+    // AI2 is ignition switch
+    // GPIO_NUM_35
+    
+    //
+    gpio_config_t io_conf = {};
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    
+
+}
 
 void app_main(){
     printf("[*] GuduVCU start-up procedure...");
 
+    // dump I/O settings
+    gpio_dump_io_configuration(stdout, SOC_GPIO_VALID_GPIO_MASK);
+    
     fflush(stdout);
     return;
 }
